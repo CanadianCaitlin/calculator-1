de7be10e2b83c22257374235f1a54fe7b0dc94ed
Caitlin Wright <misscaitlinawright@gmail.com>
2020-07-18 18:45:47 +0000

    completed cube function

--- arithmetic.py
+++ arithmetic.py
@@ -21,11 +21,12 @@ def divide(num1, num2):
 
 def square(num1):
     """Return the square of the input."""
+    return num1 * num1
 
 
 def cube(num1):
     """Return the cube of the input."""
-
+    return num1 ** 3
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
